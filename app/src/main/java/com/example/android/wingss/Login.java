5bdcf10ddd6f9d4b7620b211018e28eb4447cc1c
ninaad <ninaad61348@gmail.com>
2018-05-21 12:32:48 +0530

    password strength progress bar added

--- app/src/main/java/com/example/android/wingss/Login.java
+++ app/src/main/java/com/example/android/wingss/Login.java
@@ -17,6 +17,7 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,6 +38,8 @@ public class Login extends AppCompatActivity {
     Button ver_btn;
     LinearLayout layout;
     SQLiteDatabase database;
+    ProgressBar pb;
+    TextView pwd_check_view;
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
     @Override
@@ -72,8 +75,8 @@ public class Login extends AppCompatActivity {
         layout=(LinearLayout)dialogs.findViewById(R.id.lays);
 
         database = new Dbhelper(this).getReadableDatabase();
-
-
+        pb=(ProgressBar)dialogs.findViewById(R.id.pbar);
+        pwd_check_view=(TextView)findViewById(R.id.pwd_view);
         ver_btn=(Button)findViewById(R.id.versatile1);
         pwd_d.setText(null);
         pwd_con_d.setText(null);
@@ -211,7 +214,7 @@ public class Login extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-
+                check_pwd(s.toString());
             }
 
             @Override
@@ -358,4 +361,135 @@ public class Login extends AppCompatActivity {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(emailStr);
         return matcher.find();
     }
+    public void check_pwd(String s) {
+
+
+        String temp = s;
+
+        int length = 0, uppercase = 0, lowercase = 0, digits = 0, symbols = 0, bonus = 0, requirements = 0;
+
+        int lettersonly = 0, numbersonly = 0, cuc = 0, clc = 0;
+
+        length = temp.length();
+        for (int i = 0; i < temp.length(); i++) {
+            if (Character.isUpperCase(temp.charAt(i)))
+                uppercase++;
+            else if (Character.isLowerCase(temp.charAt(i)))
+                lowercase++;
+            else if (Character.isDigit(temp.charAt(i)))
+                digits++;
+
+            symbols = length - uppercase - lowercase - digits;
+
+        }
+
+        for (int j = 1; j < temp.length() - 1; j++) {
+
+            if (Character.isDigit(temp.charAt(j)))
+                bonus++;
+
+        }
+
+        for (int k = 0; k < temp.length(); k++) {
+
+            if (Character.isUpperCase(temp.charAt(k))) {
+                k++;
+
+                if (k < temp.length()) {
+
+                    if (Character.isUpperCase(temp.charAt(k))) {
+
+                        cuc++;
+                        k--;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        for (int l = 0; l < temp.length(); l++) {
+
+            if (Character.isLowerCase(temp.charAt(l))) {
+                l++;
+
+                if (l < temp.length()) {
+
+                    if (Character.isLowerCase(temp.charAt(l))) {
+
+                        clc++;
+                        l--;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        System.out.println("length" + length);
+        System.out.println("uppercase" + uppercase);
+        System.out.println("lowercase" + lowercase);
+        System.out.println("digits" + digits);
+        System.out.println("symbols" + symbols);
+        System.out.println("bonus" + bonus);
+        System.out.println("cuc" + cuc);
+        System.out.println("clc" + clc);
+
+        if (length > 7) {
+            requirements++;
+        }
+
+        if (uppercase > 0) {
+            requirements++;
+        }
+
+        if (lowercase > 0) {
+            requirements++;
+        }
+
+        if (digits > 0) {
+            requirements++;
+        }
+
+        if (symbols > 0) {
+            requirements++;
+        }
+
+        if (bonus > 0) {
+            requirements++;
+        }
+
+        if (digits == 0 && symbols == 0) {
+            lettersonly = 1;
+        }
+
+        if (lowercase == 0 && uppercase == 0 && symbols == 0) {
+            numbersonly = 1;
+        }
+
+        int Total = (length * 4) + ((length - uppercase) * 2)
+                + ((length - lowercase) * 2) + (digits * 4) + (symbols * 6)
+                + (bonus * 2) + (requirements * 2) - (lettersonly * length * 2)
+                - (numbersonly * length * 3) - (cuc * 2) - (clc * 2);
+
+        System.out.println("Total" + Total);
+
+        if (Total < 30) {
+            pb.setProgress(Total - 15);
+        } else if (Total >= 40 && Total < 50) {
+            pb.setProgress(Total - 20);
+        } else if (Total >= 56 && Total < 70) {
+            pb.setProgress(Total - 25);
+        } else if (Total >= 76) {
+            pb.setProgress(Total - 30);
+        } else {
+            pb.setProgress(Total - 20);
+        }
+    }
+
 }
+
